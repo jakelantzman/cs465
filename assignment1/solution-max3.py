1b6e0161651fa4b49de095bcd5e7f63bdbaca330
max-molden <57696611+max-molden@users.noreply.github.com>
2020-09-05 21:09:14 -0400

    Update solution-max3.py

--- assignment1/solution-max3.py
+++ assignment1/solution-max3.py
@@ -3,9 +3,9 @@ Team Members: Maxwell Molden, Jacob Lantzman, Spencer Leahy
 '''
 
 import fileinput
-# The mergeSortedArrays function will take in two arrays that are already sorted and produce an array that is the sorted, merged
-# result of the two sorted arrays
 
+# The castToInt() function will take in the numbers that were read into an array from the input file along with the length read from the input
+# file; it then iterates over the array and changes the type of each element to int and the type of the length to int and returns both
 def castToInt(starting_arr, starting_len):
     starting_len = int(starting_len)
 
@@ -14,12 +14,15 @@ def castToInt(starting_arr, starting_len):
 
     return starting_arr, starting_len
 
-
+# The mergeSortedArrays() function will take in two arrays that are already sorted and produce an array that is the sorted, merged
+# result of the two sorted arrays
 def mergeSortedArrays(left_arr, right_arr): 
     out_arr = []
 
+    # counter variables to track the index of the elements for comparisions and append()
     left_count = 0
     right_count = 0
+
     # Loop through each array and evaluate at each index which integer to insert into the output array,
     # if the length of either array becomes zero throughout the loop, append all remaining integers from
     # the other array to the output array
@@ -27,9 +30,11 @@ def mergeSortedArrays(left_arr, right_arr):
         if(left_arr[left_count] < right_arr[right_count]):
             out_arr.append(left_arr[left_count])
             left_count += 1
+
         elif(left_arr[left_count] > right_arr[right_count]):
             out_arr.append(right_arr[right_count])
             right_count += 1
+
         elif(left_arr[left_count] == right_arr[right_count]):
             out_arr.append(left_arr[left_count])
             out_arr.append(right_arr[right_count])
@@ -39,41 +44,34 @@ def mergeSortedArrays(left_arr, right_arr):
     if len(left_arr) == left_count:
         for i in right_arr[right_count:]:
             out_arr.append(int(i))
+
     elif len(right_arr) == right_count:
         for i in left_arr[left_count:]:
             out_arr.append(int(i))
 
-    # print(f"out arr {out_arr}")
-
     return out_arr
 
+# the mergeSort() function takes in an array along with its size, and the (initially empty) resultant array. It follows the merge-sort algorithm
+# to split an array of n elements into n single-element-arrays and recombines them using the mergeSortedArrays() funciton.  mergeSort() is called
+# recursively in order to continue splitting the arrays into smaller arrays.  If the array length to start is <= 1, the array is already sorted
+# and will be returned
 def mergeSort(orig_array, orig_size, sort_arr): 
-    orig_size = int(orig_size)
-
     if orig_size > 1:
+        # splitting the arrays as close to in half as possible, done recursively
         midpoint = orig_size // 2
         leftArray = orig_array[:midpoint]
         rightArray = orig_array[midpoint:]
 
-        # print(f"left before: {leftArray}")
-        # print(f"rigth before: {rightArray}")
-        # print(f"type left: {type(leftArray)}")
-        # print(f"type right: {type(rightArray)}")
-
         left_len = len(leftArray)
         right_len = len(rightArray)
 
-        # print(f"left len: {left_len}")
-        # print(f"rigth len: {right_len}")
-
-        leftArray, rightArray = mergeSort(leftArray, left_len, sort_arr), mergeSort(rightArray, right_len, sort_arr)
+        mergeSort(leftArray, left_len, sort_arr)
+        mergeSort(rightArray, right_len, sort_arr)
 
         sort_arr = mergeSortedArrays(leftArray, rightArray)
+
     else:
         return sort_arr
-
-
-        #print(sort_arr)
 
     return sort_arr
     
